19ed65d7e468f9ec37f2d19008bde486b7da89c4
developercriscr <crislydevelopercr@gmail.com>
2026-05-30 14:39:51 -0600

    Documentamos jerarquia de excepciones y manejo de errores

---
+++ src/utilidades/PruebaExcepciones.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utilidades;
+
+import modelo.DatoInvalidoException;
+
+/**
+ *
+ * @author jpiru
+ * /*
+Jerarquía básica de excepciones en Java:
+
+Throwable
+    Exception
+          RuntimeException
+                Excepciones específicas
+
+DatoInvalidoException hereda de Exception,
+por lo tanto es una excepción verificada
+(checked exception) y debe manejar con
+try-catch o declararse con throws.
+*
+ * 
+ * 
+ * 
+ */
+public class PruebaExcepciones {
+    public static void probarValidacion() {
+
+        try {
+
+            Validador.validarNombre("");
+
+        } catch (DatoInvalidoException e) {
+
+            System.out.println(
+                    "Error capturado: "
+                    + e.getMessage()
+            );
+
+        }
+
+    }
+    
+    public static void validarPersona(
+        String nombre,
+        String identificacion
+) throws DatoInvalidoException {
+
+    try {
+
+        Validador.validarNombre(nombre);
+        Validador.validarIdentificacion(identificacion);
+
+    } catch (DatoInvalidoException e) {
+
+        throw new DatoInvalidoException(
+                "Error al validar persona: "
+                + e.getMessage()
+        );
+
+    }
+}
+}
